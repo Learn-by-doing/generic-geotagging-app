686b612c6d9a3117b6610ae66a96ea0ef0c0493d
lopliok <lopliok.lopliok@gmail.com>
2018-07-02 19:54:08 +0200

    library link changed back

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-//import '../node_modules/leaflet/dist/leaflet.css'
+import '../node_modules/leaflet/dist/leaflet.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
